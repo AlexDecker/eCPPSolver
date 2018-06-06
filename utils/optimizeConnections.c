0f2134eecea93e55d1c3b58e97ab6256191d0c56
AlexDecker <alexanderdecker@dcc.ufmg.br>
2018-06-06 17:20:17 -0300

    start heuristics

---
+++ utils/optimizeConnections.c
@@ -0,0 +1,1 @@
+#include "optimizeConnections.h"
---
+++ utils/switch.h
@@ -0,0 +1,13 @@
+#include <stdlio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct{
+	double requestFrequency;
+	double energyPerRequest;
+	double procTimePerRequest;
+}switch;
+
+typedef struct{
+	
+}
