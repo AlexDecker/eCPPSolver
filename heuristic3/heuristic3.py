c69cb2f6c1bba5bee0da9ec77811884ffc60a617
AlexDecker <alexdeckersousa@hotmail.com>
2018-06-24 19:28:43 +0000

    heuristic3

---
+++ heuristic3/heuristic3.py
@@ -0,0 +1,66 @@
+import sys
+sys.path.insert(0,'../utils')
+import eCPPGraph
+import eCPPDominantingSet
+import evaluateSolution
+
+def run(graph):
+	#if all bad edges (whose latency is more than this threshold)
+	#were removed, the solution is certainly feasible
+	if len(graph.nodeList)>1:
+		threshold = graph.maxTotalLatency/(len(graph.nodeList)-1)
+	else:
+		#there is no edge, so this value is irrelevant
+		threshold = 0
+
+	#lists of edges from eCPPGraph
+	blackList = []
+	badEdgeList = []
+	for n in graph.nodeList:
+		for e in n._neighborhood:
+			if e.latency>threshold:
+				badEdgeList.append(e)	
+	badEdgeList.sort(key=lambda item: item.latency, reverse=True)
+	
+	while True:
+		#prepare the new subgraph for execution
+		graph.makeSandBox()
+		#eliminate some bad edges
+		for e in blackList:
+			graph.nodeList[e.fromNode].neighborhood_sandbox.remove(e)
+			graph.nodeList[e.toNode].neighborhood_sandbox.remove(\
+				e.returnEdge)
+		
+		placementVector,assingMatrix = eCPPDominantingSet.solve(\
+			graph)
+		value,feasible = evaluateSolution.eval(placementVector,\
+			assingMatrix,graph)
+		if not feasible:
+			#assume it is bacause of the latency constraint
+			if badEdgeList!=[]:
+				#put the worst edge in the blacklist
+				e = badEdgeList[0]
+				badEdgeList.remove(e)
+				try:
+					badEdgeList.remove(e.returnEdge)
+				except ValueError:
+					pass #just ignore
+				blackList.append(e)
+			else:
+				print 'Something went wrong'
+				return value, False
+		else:
+			break
+	return value, feasible, placementVector, assingMatrix
+
+def test(file = '../testScripts/autoGen.xml', nSFreq = 10,\
+	nSLat = 10):
+	while True:
+		g = eCPPGraph.graph(file,\
+			nSamplesFreq=nSFreq,nSamplesLat=nSLat)
+		
+		value,feasible,_,_ = run(g)
+		if feasible:
+			print value
+		else:
+			exit()
