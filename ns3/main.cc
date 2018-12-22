dde5ea13e11c69590a3e160e3006eeb692b48a5d
AlexDecker <alexdeckersousa@hotmail.com>
2018-12-22 12:50:29 +0000

    working - i guess - but not entirely tested

--- ns3/main.cc
+++ ns3/main.cc
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <vector>
+#include <algorithm>    // std::sort
 
 /*
  * Endereçamento: 10.X.Y.Z
@@ -19,11 +20,29 @@ using std::vector;
 
 NS_LOG_COMPONENT_DEFINE ("simulacaoSBRC");
 
+//payload da mensagem da primeira parte do algoritmo distribuído
 typedef struct{
 	int fromId;
 	int ownerId;
 	double demand;
 }PayLoad;
+
+//estruturas para a segunda parte do algoritmo de controle de topologia
+typedef struct{
+	double demand;//demanda do nó a que este link se direciona
+	double propagationTime;
+	int to;
+}TCLink;
+
+typedef struct{
+	double residualCapacity;
+	double weight;
+	int parent;
+	bool hasController;
+	vector<TCLink> neighborhood;
+}TCNode;
+
+//Estrutura para a simulação dos enlaces físicos da wan
 
 typedef struct{
 	//para a contagem da energia gasta
@@ -38,6 +57,14 @@ typedef struct{
 	//index dos nós nas listas correspondentes
 	int id[2];
 }ControlLink;
+
+//Funções para a comparação em sorts
+bool compPropagation(ControlLink i, ControlLink j){
+	return(i.propagationTime>j.propagationTime);
+}
+bool compDemand(TCLink i, TCLink j){
+	return(i.demand>j.demand);
+}
 
 class Controller{
 	public:
@@ -78,6 +105,7 @@ class Controller{
 		void initializeTopologyAlgorithm();//começa a divulgar os parâmetros
 		void cpMessageHandler(Address from, Ptr<Packet> packet);//disseminação de parâmetros
 		void recognizeAsChild(int routerId);//envia uma notificação de paternidade
+		void posicionadorRelaxado(TCNode* tcNodes);//heurística que não considera a restrição de latência
 		void heuristica1();//realiza de fato a otimização
 		////
 	private:
@@ -121,7 +149,7 @@ class Wan{
 	private:
 		vector<ControlLink> controlPlaneLinks;
 		vector<ControlLink> southBoundLinks;
-		vector<Controller*> controllers;
+		
 		NodeContainer nodes;
 		void addSouthBoundLink(double joulesPerBit, double propagationTime,
 			int controller, int router);
@@ -131,7 +159,10 @@ class Wan{
 		Router* getRouterFromControllerIP(Address addr);
 		Controller* getControllerFromControllerIP(Address addr);
 	public:
+		vector<Controller*> controllers;
 		vector<Router*> routers;
+		
+		double maxControlLatency;
 		
 		void addLocation(Controller* ctrl, Router* rtr);
 		void installIpv4();
@@ -144,6 +175,9 @@ class Wan{
 		void handleCPMessage(Address from, Ptr<Packet> packet);
 		void defineParents();//inicia o algoritmo distribuído nos controladores
 		void printStatistics();
+		double limitToCut(int nEdgesToCut);//[nEdgesToCut]º maior tempo de propagação
+		double totalControlLatency(TCNode* tcNodes);//latência total dada uma configuração
+		//topológica
 };
 
 Wan wan;//gloabal para facilitar os handlers
@@ -172,6 +206,8 @@ int main(int argc, char** argv){
 	Time::SetResolution (Time::NS);
 	srand((unsigned)time(NULL));
 	
+	wan.maxControlLatency = 1;//latência de todos os links deve somar 1s no máximo
+	
 	int nLocations = 2;
 	uint32_t msgSize = 1024;//1kb<-Importante, esse valor não pode ser 1. Veja responseHandler
 	//para melhores explicações
@@ -181,9 +217,9 @@ int main(int argc, char** argv){
 	//intervalo de envio dentro de um mesmo dispositivo (s)
 	double interval = 0.0000001*msgSize*8;//Vazões de no máximo 10Gbps
 	
-	double capacity = 2;//2 requests por s
+	double capacity = 2000000/(msgSize*8);//2Gbps
 	double responseProbability = 0.5;//50% de chance
-	double processingEnergy = 0.000000033;//J/mensagem
+	double processingEnergy = 0.00033;//J/mensagem
 	double fixedEnergy = 600;//600W
 	double energyPrice = 0.000000033;//$/J -> 0.12/kWh
 	double requestProbability = 0.5;//50% de chance
@@ -365,8 +401,159 @@ void Controller::recognizeAsChild(int routerId){
 	}
 }
 
+//heurística sem considerar a restrição de latência
+void Controller::posicionadorRelaxado(TCNode* tcNodes){
+	int i,j,k;
+	int ndNodes = nLocations;//no início, nenhum nó está dominado
+	while(ndNodes>0){
+		//buscando o nó de maior eficiência, o index que a maximiza e a eficiência
+		//em si
+		int node, ind; double maxEff = 0;
+		for(i=0;i<nLocations;i++){
+			if(!tcNodes[i].hasController){//busque instalar um controlador se não houver um aqui
+				double demandSum;
+				if(tcNodes[i].parent==-1){
+					demandSum = demands[i];//se não foi dominado ainda, deve atender a própria demanda
+				}else{
+					demandSum = 0;
+				}
+				//verificando quantos nós pode atender
+				for(j=0;j<(int)tcNodes[i].neighborhood.size();j++){
+					if(demandSum+tcNodes[i].neighborhood[j].demand<=tcNodes[i].residualCapacity){
+						demandSum+=tcNodes[i].neighborhood[j].demand;
+					}else{
+						break;
+					}
+				}
+				//verificando se a eficiência é maior do que a que já se tinha conhecimento
+				if(maxEff<=j/tcNodes[i].weight){
+					maxEff = j/tcNodes[i].weight;
+					ind = j;//o penúltimo valor de j foi o último a respeitar a capacidade residual 
+					node = i;
+				}
+			}
+		}
+		
+		//O nó de maior eficiência passa a ter um controlador
+		tcNodes[node].hasController = true;
+		
+		//os ind primeiros nós são dominados por node
+		for(i=0;i<ind;i++){
+			tcNodes[node].residualCapacity -= tcNodes[node].neighborhood[i].demand;
+			tcNodes[tcNodes[node].neighborhood[i].to].parent = node;
+			ndNodes--;
+			//retirando esse nó das vizinhanças de outros nós
+			for(j=0;j<nLocations;j++){
+				if(j!=node){
+					k=0;
+					while(k<(int)tcNodes[j].neighborhood.size()){
+						//verificando se é para um nó dominado
+						if(tcNodes[j].neighborhood[k].to==tcNodes[node].neighborhood[i].to){
+							//se sim, retire-o. Não avance o apontador, a remoção deste valor
+							//trará o próximo
+							tcNodes[j].neighborhood.erase(tcNodes[j].neighborhood.begin()+k);
+						}else{
+							//senão, avance o apontador
+							k++;
+						}
+					}
+				}
+			}
+		}
+		
+		//eliminando os nós dominados por node
+		for(i=0;i<ind;i++){
+			tcNodes[node].neighborhood.erase(tcNodes[node].neighborhood.begin());
+		}
+		
+		//se node ainda não foi dominado
+		if(tcNodes[node].parent==-1){
+			tcNodes[node].residualCapacity -= demands[node];
+			tcNodes[node].parent = node;
+			ndNodes--;
+			//retirando esse nó das vizinhanças de outros nós
+			for(j=0;j<nLocations;j++){
+				if(j!=node){
+					k=0;
+					while(k<(int)tcNodes[j].neighborhood.size()){
+						//verificando se é para o nó em questão
+						if(tcNodes[j].neighborhood[k].to==node){
+							//se sim, retire-o. Não avance o apontador, a remoção deste valor
+							//trará o próximo
+							tcNodes[j].neighborhood.erase(tcNodes[j].neighborhood.begin()+k);
+						}else{
+							//senão, avance o apontador
+							k++;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 void Controller::heuristica1(){
-	recognizeAsChild(1-ID);
+	int i,j;
+	int nEdgesToCut = 0;
+	int x = 2;
+	while(x--){
+		//valor de tempo de propagação a partir do qual as arestas serão cortadas
+		//(considero aqui, por motivo de simplificação, que os valores não se repetem.
+		//caso se repitam, diferencie por algum valor infinitesimal)
+		double lim = wan.limitToCut(nEdgesToCut);
+		
+		//preparando as estruturas para a próima iteração
+		TCNode tcNodes[nLocations];
+		for(i=0;i<nLocations;i++){
+			//inicial
+			tcNodes[i].parent = -1;
+			tcNodes[i].hasController = false;
+			tcNodes[i].residualCapacity = wan.controllers[i]->capacity;
+			tcNodes[i].weight = wan.controllers[i]->energyPrice
+				*(wan.controllers[i]->fixedEnergy
+				+ demands[i] * wan.controllers[i]->processingEnergy);
+			tcNodes[i].neighborhood.clear();//assegurando que está vazio
+			for(j=0;j<(int)wan.controllers[i]->southBoundLinks.size();j++){
+				//verificando se o link é para uma localidade externa ao controlador i
+				//verificando também se há um limite de tempo de propagação a ser obedecido
+				//(lim!=-1) e o enlace obedece esse limite
+				if((wan.controllers[i]->southBoundLinks[j].id[1]!=wan.controllers[i]->ID)
+					&&((wan.controllers[i]->southBoundLinks[j].propagationTime<lim)
+					|| lim==-1)){
+					
+					TCLink link;
+					
+					link.to = wan.controllers[i]->southBoundLinks[j].id[1];//id do nó a que esse link leva
+					link.demand = demands[link.to];
+					link.propagationTime = wan.controllers[i]->southBoundLinks[j].propagationTime;
+					tcNodes[i].neighborhood.push_back(link);
+				}
+			}
+		}
+		/*
+		//Verificando dados
+		for(i=0;i<nLocations;i++){
+			//inicial
+			NS_LOG_UNCOND("cap:"<<tcNodes[i].residualCapacity<<"; peso: "<<tcNodes[i].weight<<"; demand: "<<demands[i]);
+			for(j=0;j<(int)tcNodes[i].neighborhood.size();j++){
+				NS_LOG_UNCOND("  pt:"<<tcNodes[i].neighborhood[j].propagationTime<<"; demand:"<<tcNodes[i].neighborhood[j].demand);
+			}
+		}
+		NS_LOG_UNCOND("\\");*/
+		
+		posicionadorRelaxado(tcNodes);
+		
+		if(wan.totalControlLatency(tcNodes)<=wan.maxControlLatency){
+			for(i=0;i<nLocations;i++){
+				if(tcNodes[i].parent==ID){
+					recognizeAsChild(i);
+				}
+			}
+			break;
+		}else{
+			nEdgesToCut++;
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -740,4 +927,40 @@ static void GenerateTraffic(uint32_t pktCount, Time pktInterval){
 static void TopologyManager(){
 	NS_LOG_UNCOND("INICIANDO GERENCIADOR DE TOPOLOGIA");
 	wan.defineParents();
+}
+
+//[nEdgesToCut]º maior tempo de propagação
+double Wan::limitToCut(int nEdgesToCut){
+	int i;
+	vector<ControlLink> links;
+	if(nEdgesToCut==0) return -1;
+	else{
+		//copiando o vetor (para manter a ordem do original)
+		for(i=0;i<(int)southBoundLinks.size();i++){
+			links.push_back(southBoundLinks[i]);
+		}
+		//ordenando por tempo de propagação
+		std::sort(links.begin(), links.end(), compPropagation);
+		
+		return(links[nEdgesToCut-1].propagationTime);
+	}
+}
+
+double Wan::totalControlLatency(TCNode* tcNodes){
+	int i,j;
+	double lat = 0;
+	//o tamanho de 'routers' é usado por ser exatamente o número de localidades
+	for(i=0;i<(int)routers.size();i++){
+		//links dentro de uma mesma localidade tem tempo de propagação 0
+		if(tcNodes[i].parent != i){
+			//encontrando o valor de tempo de propagação do link
+			for(j=0;j<(int)southBoundLinks.size();j++){
+				if((southBoundLinks[j].id[0]==tcNodes[i].parent)
+					&&(southBoundLinks[j].id[1]==1)){
+					lat += southBoundLinks[j].propagationTime;
+				}
+			}
+		}
+	}
+	return lat;
 }
